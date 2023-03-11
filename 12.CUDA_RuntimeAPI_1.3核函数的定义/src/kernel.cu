7007cea9a01d1f8fe62c162efa19c044480eab47
cqy <1642942273@qq.com>
2023-03-11 16:02:19 +0800

    git@github.com:PredyDaddy/DL_Deploy.git

--- 12.CUDA_RuntimeAPI_1.3核函数的定义/src/kernel.cu
+++ 12.CUDA_RuntimeAPI_1.3核函数的定义/src/kernel.cu
@@ -3,13 +3,15 @@
 #include <math.h>
 
 // 全部的函数 exp std::io 英伟达GPU都封装好了
-__device__ __host__ float sigmoid(float x){
+__device__ __host__ float sigmoid(float x)
+{
     return 1 / (1 + exp(-x));
 }
 
-__global__ void test_print_kernel(const float* pdata, int ndata){
+__global__ void test_print_kernel(const float *pdata, int ndata)
+{
 
-    // 内置变量 
+    // 内置变量
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     /*    dims                 indexs
         gridDim.z    1        blockIdx.z      0
@@ -17,7 +19,7 @@ __global__ void test_print_kernel(const float* pdata, int ndata){
         gridDim.x    1        blockIdx.x      0
         blockDim.z   1        threadIdx.z     0
         blockDim.y   1        threadIdx.y     0
-        blockDim.x   10        threadIdx.x    0-9
+        blockDim.x   10       threadIdx.x    0-9
 
         Pseudo code:
         position = 0
@@ -30,7 +32,8 @@ __global__ void test_print_kernel(const float* pdata, int ndata){
 }
 
 // 这个__host__写不写都是一样的，他就是一个设备函数
-__host__ void test_print(const float* pdata, int ndata){
+__host__ void test_print(const float *pdata, int ndata)
+{
 
     float y = sigmoid(0.5f);
     // <<<gridDim, blockDim, bytes_of_shared_memory, stream>>>
@@ -42,7 +45,7 @@ __host__ void test_print(const float* pdata, int ndata){
     // 为什么补nthreads = 10; ?
     // 我们会遇到多维度的问题，
     // 这些是两个Dim的极限了
-    // gridDim(21亿, 65536, 65536) 
+    // gridDim(21亿, 65536, 65536)
     // blockDim(1024, 64, 64) blockDim.x * blockDim.y * blockDim.z <= 1024;
 
     // nullptr这里是默认流，想要异步操作就放个stream
@@ -56,9 +59,10 @@ __host__ void test_print(const float* pdata, int ndata){
     // 而cudaPeekAtLastError是获取当前错误，但是再一次执行 cudaPeekAtLastError 或者 cudaGetLastError 拿到的还是那个错
     // cuda的错误会传递，如果这里出错了，不移除。那么后续的任意api的返回值都会是这个错误，都会失败
     cudaError_t code = cudaPeekAtLastError();
-    if(code != cudaSuccess){    
-        const char* err_name    = cudaGetErrorName(code);    
-        const char* err_message = cudaGetErrorString(code);  
-        printf("kernel error %s:%d  test_print_kernel failed. \n  code = %s, message = %s\n", __FILE__, __LINE__, err_name, err_message);   
+    if (code != cudaSuccess)
+    {
+        const char *err_name = cudaGetErrorName(code);
+        const char *err_message = cudaGetErrorString(code);
+        printf("kernel error %s:%d  test_print_kernel failed. \n  code = %s, message = %s\n", __FILE__, __LINE__, err_name, err_message);
     }
 }
